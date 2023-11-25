bd28a765d9b89c7a2c0715faf048cbcfb63493e5
Sohaib <74816112+Sohaib-2@users.noreply.github.com>
2023-11-25 09:29:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,10 @@ import spacy
 # Download NLTK resources
 nltk.download('punkt')
 
-# Load spaCy model for entity recognition
-spacy.download('en_core_web_sm')
-nlp = spacy.load("en_core_web_sm")
-
 def generate_ngrams(text, n):
     words = nltk.word_tokenize(text)
     n_grams = list(ngrams(words, n))
     return n_grams
-
-def extract_entities(text):
-    doc = nlp(text)
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
-    return entities
 
 # Streamlit web application
 def main():
@@ -33,13 +24,5 @@ def main():
         if input_text:
             ngrams_result = generate_ngrams(input_text, n_value)
             st.write(f"{n_value}-grams:", ngrams_result)
-        else:
-            st.warning("Please enter some text.")
-
-    # Entity recognition
-    if st.button("Extract Entities"):
-        if input_text:
-            entities_result = extract_entities(input_text)
-            st.write("Entities:", entities_result)
         else:
             st.warning("Please enter some text.")
